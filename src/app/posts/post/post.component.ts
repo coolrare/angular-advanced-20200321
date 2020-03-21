e8b997961e49046071430f57d762c9905ca5b559
Mike Huang <wellwind@mail2000.com.tw>
2020-03-21 11:02:42 +0800

    任務04：在程式中取得路由參數

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../post.service';
+import { Article } from '../article';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  article: Article;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private articleService: PostService) { }
 
   ngOnInit() {
+    // this.route.params.subscribe(params => {
+    //   console.log(params.id);
+    // });
+
+    this.route.paramMap.subscribe(params => {
+      this.articleService.getArticle(params.get('id')).subscribe(article => {
+        this.article = article.article;
+      });
+    });
   }
 
 }
--- src/app/posts/posts/posts.component.ts
+++ src/app/posts/posts/posts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { Articles, Article } from '../article';
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
+  articles: Article[];
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
+    this.postService.getArticles().subscribe(articles => {
+      this.articles = articles.articles;
+      console.log(this.articles);
+    });
   }
 
 }
